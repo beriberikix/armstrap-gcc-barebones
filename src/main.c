15f3657ca4609738d697d2b3e5c20c74f3d91c00
Jonathan Beri <jmberi@gmail.com>
2015-06-11 07:44:37 -0700

    toggle direction

--- src/main.c
+++ src/main.c
@@ -4,39 +4,115 @@ const uint16_t LEDS = GPIO_Pin_1;
 const uint16_t LED[1] = {GPIO_Pin_1};
 const long LED_COUNT = 1;
 
-int main()
-{
-    init();
-    do
-    {
-        loop();
-    }
-    while (1);
-
-    return 0; // never reached
-}
+TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+TIM_OCInitTypeDef  TIM_OCInitStructure;
+uint16_t CCR1_Val = 333;
+uint16_t CCR2_Val = 249;
+uint16_t CCR3_Val = 166;
+uint16_t CCR4_Val = 83;
+uint16_t PrescalerValue = 0;
 
 void loop()
 {
-    uint32_t counter = 0;
+//    uint32_t counter = 0;
+
+/* Compute the prescaler value */
+  PrescalerValue = (uint16_t) ((SystemCoreClock /2) / 21000000) - 1;
+
+  /* Time base configuration */
+  TIM_TimeBaseStructure.TIM_Period = 665;
+  TIM_TimeBaseStructure.TIM_Prescaler = PrescalerValue;
+  TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+
+  TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
+
+  /* PWM1 Mode configuration: Channel1 */
+  TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = CCR1_Val;
+  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+
+  TIM_OC1Init(TIM3, &TIM_OCInitStructure);
+
+  TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Enable);
+
+  /* PWM1 Mode configuration: Channel2 */
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = CCR1_Val;
+
+  TIM_OC2Init(TIM3, &TIM_OCInitStructure);
+
+  TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Enable);
+
+  /* PWM1 Mode configuration: Channel3 */
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = CCR1_Val;
+
+  TIM_OC3Init(TIM3, &TIM_OCInitStructure);
+
+  TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Enable);
+
+  /* PWM1 Mode configuration: Channel4 */
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = CCR1_Val;
+
+  TIM_OC4Init(TIM3, &TIM_OCInitStructure);
+
+  TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Enable);
+
+  TIM_ARRPreloadConfig(TIM3, ENABLE);
+
+  /* TIM3 enable counter */
+  TIM_Cmd(TIM3, ENABLE);
+
+
     while (1) {
-        counter++;
-        GPIO_ToggleBits(GPIOC, GPIO_Pin_1);
-        delay(500);
+       GPIO_ToggleBits(GPIOA, GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10 | GPIO_Pin_11);
+       delay(3000);
     };
 }
 
+void pwmInit(void) {
+  GPIO_InitTypeDef GPIO_InitStructure;
 
-void init() {
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+  /* TIM3 clock enable */
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+
+  /* GPIOC clock enable */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+  
+  /* GPIOC Configuration: TIM3 CH1 (PC6), TIM3 CH2 (PC7), TIM3 CH3 (PC8) and TIM3 CH4 (PC9) */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP ;
+  GPIO_Init(GPIOC, &GPIO_InitStructure); 
+
+  /* Connect TIM3 pins to AF2 */  
+  GPIO_PinAFConfig(GPIOC, GPIO_PinSource6, GPIO_AF_TIM3);
+  GPIO_PinAFConfig(GPIOC, GPIO_PinSource7, GPIO_AF_TIM3); 
+  GPIO_PinAFConfig(GPIOC, GPIO_PinSource8, GPIO_AF_TIM3);
+  GPIO_PinAFConfig(GPIOC, GPIO_PinSource9, GPIO_AF_TIM3); 
+}
+
+void dirInit(void) {
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
     GPIO_InitTypeDef gpio;
     GPIO_StructInit(&gpio);
 
     gpio.GPIO_Mode = GPIO_Mode_OUT;
-    gpio.GPIO_Pin = LEDS;
+    gpio.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10 | GPIO_Pin_11;
 
-    GPIO_Init(GPIOC, &gpio);
-    GPIO_SetBits(GPIOC, LEDS);
+    GPIO_Init(GPIOA, &gpio);
+    GPIO_SetBits(GPIOA, GPIO_Pin_8 | GPIO_Pin_10);
+    GPIO_ResetBits(GPIOA, GPIO_Pin_9 | GPIO_Pin_11);
+}
+
+void setup(void) {
+    pwmInit();
+    dirInit();    
 }
 
 
@@ -45,4 +121,14 @@ void delay(uint32_t ms) {
     while(ms--) {
         __NOP();
     }
+}
+
+int main()
+{
+
+    setup();
+
+    loop();
+
+    return 0; // never reached
 }
--- src/main.h
+++ src/main.h
@@ -1,9 +1,12 @@
 #include "stm32f4xx.h"
 #include "stm32f4xx_gpio.h"
 #include "stm32f4xx_rcc.h"
+#include "stm32f4xx_tim.h"
 
 void init();
 
 void delay(uint32_t ms);
 
 void loop();
+
+void pwm();
